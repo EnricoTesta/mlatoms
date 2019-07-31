e153ff8897a5a58f05cf47f1f2ea45d81ad4e299
Enrico Testa <enrico@mailtesta.com>
2019-07-31 20:31:43 +0200

    Renaming and folder refactoring

--- bash_create_atoms.sh
+++ bash_create_atoms.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+cd atoms
+
 export PATH=~/google-cloud-sdk/bin:$PATH
+export PROJECT_ID=$(gcloud config list project --format "value(core.project)")
 # --train-files /NumeraiData/Numerai_test/sample_data.csv
 
 # Logistic Regression
-
-export PROJECT_ID=$(gcloud config list project --format "value(core.project)")
-export IMAGE_REPO_NAME=sklearn_logisticregression
-export IMAGE_TAG=sklearn_logreg
+export IMAGE_REPO_NAME=classification_sklearn_logisticRegression
+export IMAGE_TAG=class_skl_logReg
 export IMAGE_URI=gcr.io/$PROJECT_ID/$IMAGE_REPO_NAME:$IMAGE_TAG
 
-docker build -f LogisticRegression_DockerFile -t $IMAGE_URI ./
+docker build -f docker_logisticRegression_classifier -t $IMAGE_URI ./
 
 docker run $IMAGE_URI --C 1 --penalty l2
 
@@ -19,12 +20,11 @@ docker push $IMAGE_URI
 
 # Light Gradient Boosting Classifier
 
-export PROJECT_ID=$(gcloud config list project --format "value(core.project)")
-export IMAGE_REPO_NAME=lgbm_classifier
-export IMAGE_TAG=lgbm_classifier
+export IMAGE_REPO_NAME=classification_lgbm
+export IMAGE_TAG=class_lgbm
 export IMAGE_URI=gcr.io/$PROJECT_ID/$IMAGE_REPO_NAME:$IMAGE_TAG
 
-docker build -f lgbm_classifier_DockerFile -t $IMAGE_URI ./
+docker build -f docker_lgbm_classifier -t $IMAGE_URI ./
 
 docker run $IMAGE_URI --n_estimators 10 --max_depth 2 --debug 1
 
@@ -33,12 +33,11 @@ docker push $IMAGE_URI
 
 # XGBoost Classifier
 
-export PROJECT_ID=$(gcloud config list project --format "value(core.project)")
-export IMAGE_REPO_NAME=xgb_classifier
-export IMAGE_TAG=xgb_classifier
+export IMAGE_REPO_NAME=classification_xgboost
+export IMAGE_TAG=class_xgb
 export IMAGE_URI=gcr.io/$PROJECT_ID/$IMAGE_REPO_NAME:$IMAGE_TAG
 
-docker build -f xgb_classifier_DockerFile -t $IMAGE_URI ./
+docker build -f docker_xbg_classifier -t $IMAGE_URI ./
 
 docker run $IMAGE_URI --n_estimators 10 --max_depth 2 --debug 1
 
@@ -47,9 +46,8 @@ docker push $IMAGE_URI
 
 # Auto-sklearn Classifier
 
-export PROJECT_ID=$(gcloud config list project --format "value(core.project)")
-export IMAGE_REPO_NAME=askl_classifier
-export IMAGE_TAG=askl_classifier
+export IMAGE_REPO_NAME=classification_autosklearn
+export IMAGE_TAG=class_askl
 export IMAGE_URI=gcr.io/$PROJECT_ID/$IMAGE_REPO_NAME:$IMAGE_TAG
 
 docker build -f askl_classifier_DockerFile -t $IMAGE_URI ./
