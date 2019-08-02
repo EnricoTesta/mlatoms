2fe1f0b4d5fa0dab30ed76354a1689eb68b81c79
Enrico Testa <enrico@mailtesta.com>
2019-08-02 07:27:09 +0200

    Added deploy directory

--- deploy/gcp_docker_bridge_setup.sh
+++ deploy/gcp_docker_bridge_setup.sh
@@ -1,12 +1,15 @@
-Choose Ubuntu 18.10 (GNU/Linux 4.18.0-1005-gcp x86_64)
-add 20 GB disk + allow http and http
-set access for each API -> Storage : Read Write
+#!/bin/bash
+
+
+#Choose Ubuntu 18.10 (GNU/Linux 4.18.0-1005-gcp x86_64)
+#add 20 GB disk + allow http and http
+#set access for each API -> Storage : Read Write
 
 sudo snap remove google-cloud-sdk
 
 curl https://sdk.cloud.google.com | bash
 
-reconnect to the VM
+#reconnect to the VM
 
 install docker https://docs.docker.com/install/linux/docker-ce/ubuntu/
 sudo apt-get remove docker docker-engine docker.io containerd runc
@@ -19,10 +22,7 @@ gnupg-agent \
 software-properties-common
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
-"deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-$(lsb_release -cs) \
-stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 sudo apt-get update
 
@@ -30,8 +30,8 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io
 
 sudo docker run hello-world # test
 
-sudo usermod -a -G docker LOGIN
+sudo usermod -a -G docker <YOUR_USERNAME>
 
-reconnect to the VM
+#reconnect to the VM
 
 gcloud auth configure-docker
