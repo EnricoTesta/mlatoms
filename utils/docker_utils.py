d46fa91bfe85d1e5336c8570a7a70fed911e9bb2
Enrico Testa <enrico@mailtesta.com>
2019-08-03 16:12:41 +0200

    Start docker utils

---
+++ utils/docker_utils.py
@@ -0,0 +1,17 @@
+from yaml import safe_load
+import os
+
+
+def get_image_uri(atom_name):
+
+    # Read configuration file
+    with open(os.getcwd() + "/config/config.yml", 'r') as stream:
+        config = safe_load(stream)
+
+    # Fetch URI
+    project_id = config['PROJECT_ID']
+    image_name = config['ATOMS'][atom_name]['image_name']
+    image_tag = config['ATOMS'][atom_name]['image_tag']
+    uri = config['CONTAINER_ROOT_URL'] + project_id + "/" + image_name + ":" + image_tag
+
+    return uri
