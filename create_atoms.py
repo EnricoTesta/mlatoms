cf3d030f20b9a47f28faeea6bf875e9d702ba457
Enrico Testa <enrico@mailtesta.com>
2019-08-03 15:36:52 +0200

    python controlled atom builder

--- create_atoms.py
+++ create_atoms.py
@@ -1,9 +1,42 @@
-import pyaml
+from yaml import safe_load
+from subprocess import check_call, CalledProcessError
+import os
 
-# Read YAML configuration file
+# Read configuration file
+with open(os.getcwd() + "/config/config.yml", 'r') as stream:
+    data = safe_load(stream)
 
-# Build docker image for each atom
+successful_builds = []
+failed_builds = []
+for key in data['ATOMS'].keys():
 
-# Test atoms locally on sample data
+    # Notification
+    print("Building docker container for: %s" % key)
 
-#
+    # Build shell command
+    cmd = ["cd atoms",
+           "export PROJECT_ID=$(gcloud config list project --format \"value(core.project)\")",
+           "export IMAGE_REPO_NAME=" + data['ATOMS'][key]['image_name'],
+           "export IMAGE_TAG=" + data['ATOMS'][key]['image_tag'],
+           "export IMAGE_URI=" + data['CONTAINER_ROOT_URL'] + "$PROJECT_ID/$IMAGE_REPO_NAME:$IMAGE_TAG"
+           "sudo docker build -f " + data['ATOMS'][key]['docker'] + " -t $IMAGE_URI ./"]
+
+    # Execute command
+    try:
+        check_call(' && '.join(cmd), shell=True)
+        successful_builds.append(key)
+    except CalledProcessError as e:
+        print("Failed to execute subprocess for container %s. Return code is %s." % (key, e.returncode))
+        failed_builds.append(key)
+
+    # TODO: Test atom locally on sample data
+    pass
+
+# Summary
+print("")
+print("")
+print("--- BUILD SUMMARY ---")
+for item in successful_builds:
+    print(item + " - success")
+for item in failed_builds:
+    print(item + " - failed")
