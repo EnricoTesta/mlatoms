00758c33c271f12d29dfcd9801195a767068aa7d
Enrico Testa <enrico@mailtesta.com>
2019-08-03 16:13:00 +0200

    code refactor

--- create_atoms.py
+++ create_atoms.py
@@ -1,4 +1,5 @@
 from yaml import safe_load
+from utils.docker_utils import get_image_uri
 from subprocess import check_call, CalledProcessError
 import os
 
@@ -8,29 +9,22 @@ with open(os.getcwd() + "/config/config.yml", 'r') as stream:
 
 successful_builds = []
 failed_builds = []
-for key in data['ATOMS'].keys():
+for atom in data['ATOMS'].keys():
 
     # Notification
-    print("Building docker container for: %s" % key)
+    print("Building docker container for: %s" % atom)
 
     # Build shell command
     cmd = ["cd atoms",
-           "export PROJECT_ID=$(gcloud config list project --format \"value(core.project)\")",
-           "export IMAGE_REPO_NAME=" + data['ATOMS'][key]['image_name'],
-           "export IMAGE_TAG=" + data['ATOMS'][key]['image_tag'],
-           "export IMAGE_URI=" + data['CONTAINER_ROOT_URL'] + "$PROJECT_ID/$IMAGE_REPO_NAME:$IMAGE_TAG",
-           "sudo docker build -f " + data['ATOMS'][key]['docker'] + " -t $IMAGE_URI ./"]
+           "sudo docker build -f " + data['ATOMS'][atom]['docker_file'] + " -t " + get_image_uri(atom) + " ./"]
 
     # Execute command
     try:
-        print("")
-        print(' && '.join(cmd))
-        print("")
         check_call(' && '.join(cmd), shell=True)
-        successful_builds.append(key)
+        successful_builds.append(atom)
     except CalledProcessError as e:
-        print("Failed to execute subprocess for container %s. Return code is %s." % (key, e.returncode))
-        failed_builds.append(key)
+        print("Failed to execute subprocess for container %s. Return code is %s." % (atom, e.returncode))
+        failed_builds.append(atom)
 
     # TODO: Test atom locally on sample data
     pass
