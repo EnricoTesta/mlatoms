ad5cb8a7941bca30fa4f906b3d9fa24005bc1a20
Enrico Testa <enrico@mailtesta.com>
2019-08-03 17:45:56 +0200

    misc

--- deploy_atoms.py
+++ deploy_atoms.py
@@ -11,9 +11,12 @@ with open(os.getcwd() + "/config/atoms.yml", 'r') as stream:
 # Get permission to push to registry
 try:
     pass
-    #cmd = "cat " + get_container_registry_service_account_json() + \
-    #      " | docker login -u _json_key --password-stdin https://" + get_registry_hostname()
-    #check_call(cmd)
+    cmd = "cat " + get_container_registry_service_account_json() + \
+          " | docker login -u _json_key --password-stdin https://" + get_registry_hostname()
+    print("")
+    print(cmd)
+    print("")
+    check_call(cmd)
 except CalledProcessError as e:
     print("Failed to obtain access rights to repository. Subprocess return code is %s" % e.returncode)
     print("Aborting...")
