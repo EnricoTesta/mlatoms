fd0c532ce476df2505fd902c83b0f4f33039eac5
Enrico Testa <enrico@mailtesta.com>
2019-08-10 18:11:17 +0200

    added reminder

--- predictionApps/setup_logreg.py
+++ predictionApps/setup_logreg.py
@@ -4,3 +4,5 @@ setup(
     name='logreg-predictor',
     version='0.1',
     scripts=['predictor.py', 'logregApp/logreg_predictor.py', 'logregApp/preprocess.py'])
+
+# TODO: package build looks good, however it fails to install on GCP AI platform
