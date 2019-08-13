2f208bee621bbfc53d1e0ddec742317e12848ed5
Enrico Testa <enrico@mailtesta.com>
2019-08-13 07:05:22 +0200

    experimental refactor

--- predictionApps/experimental/setup_logreg.py
+++ predictionApps/experimental/setup_logreg.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name='logreg-predictor',
     version='0.1',
-    scripts=['predictor.py', 'logregApp/logreg_predictor.py', 'logregApp/preprocess.py'])
+    #py_modules=['templates.predictor'],
+    scripts=['predictor.py', 'preprocess.py'])
 
 # TODO: package build looks good, however it fails to install on GCP AI platform
