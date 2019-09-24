04f36ec9139b46debdcf416349229042857ffc0f
Enrico Testa <enrico@mailtesta.com>
2019-09-24 18:14:27 +0200

    generalize with group variable (ATOMS / SCORING)

--- utils/docker_utils.py
+++ utils/docker_utils.py
@@ -2,7 +2,7 @@ from yaml import safe_load
 import os
 
 
-def get_image_uri(atom_name, deployment="GCP"):
+def get_image_uri(group, name, deployment="GCP"):
 
     # Read configuration files
     with open(os.getcwd() + "/config/atoms.yml", 'r') as stream:
@@ -12,8 +12,8 @@ def get_image_uri(atom_name, deployment="GCP"):
 
     # Fetch URI
     project_id = deploy_config[deployment]['PROJECT_ID']
-    image_name = config['ATOMS'][atom_name]['image_name']
-    image_tag = config['ATOMS'][atom_name]['image_tag']
+    image_name = config[group][name]['image_name']
+    image_tag = config[group][name]['image_tag']
     uri = deploy_config[deployment]['CONTAINER_ROOT_URL'] + "/" + project_id + "/" + image_name + ":" + image_tag
 
     return uri
