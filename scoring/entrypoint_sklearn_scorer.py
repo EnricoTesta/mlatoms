abc39f2090775519ee664cb672ad49e4618a061f
Enrico Testa <enrico@mailtesta.com>
2019-09-25 05:53:53 +0200

    change use_proba default from True to 1

--- scoring/entrypoint_sklearn_scorer.py
+++ scoring/entrypoint_sklearn_scorer.py
@@ -27,7 +27,7 @@ def get_args():
         help='The directory to store output data')
     parser.add_argument(
         '--use-proba',
-        default=True,
+        default=1,
         metavar='use_proba',
         help='Flag whether or not to use probabilities.')
 
