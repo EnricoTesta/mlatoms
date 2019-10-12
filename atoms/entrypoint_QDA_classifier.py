dd98c2e9e2dde64a695bce4a14773dacd145349b
Enrico Testa <enrico@mailtesta.com>
2019-10-12 16:03:15 +0200

    add class_QDA

---
+++ atoms/entrypoint_QDA_classifier.py
@@ -0,0 +1,55 @@
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+from trainer import Trainer
+import argparse
+
+
+def get_args():
+    """Argument parser.
+    Returns:
+      Dictionary of arguments.
+    """
+    parser = argparse.ArgumentParser(description='Logistic Regression Example')
+    parser.add_argument(
+        '--reg-param',
+        type=float,
+        default=0,
+        metavar='reg_param',
+        help='Regularizes the covariance estimate as identity matrix')
+    parser.add_argument(
+        '--model-dir',
+        default='/mlatoms/test/modeldir',
+        metavar='model_dir',
+        help='The directory to store the model')
+    parser.add_argument(
+        '--train-files',
+        default='/mlatoms/data/classification/binary/',
+        metavar='train_files',
+        help='The directory to fetch train data')
+    parser.add_argument(
+        '--hypertune-loss',
+        default=None,
+        metavar='hypertune_loss',
+        help='Hypertune loss name')
+
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    # Training settings
+    args = get_args()
+    args_dict = vars(args)
+
+    # Make param dict
+    param_dict = {'algo': {}, 'fit': {}}
+    for item in args_dict:
+        if item not in ('model_dir', 'train_files', 'hypertune_loss'):
+            param_dict['algo'][item] = args_dict[item]
+
+    t = Trainer(train_data_path=args.train_files, model_path=args.model_dir, algo=QuadraticDiscriminantAnalysis,
+                params=param_dict, hypertune_loss=args.hypertune_loss)
+    t.run()
+
+
+if __name__ == '__main__':
+    main()
