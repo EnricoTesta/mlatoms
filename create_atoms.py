20a6eab2dd7bf307da64dfc3136746cbbb323ae8
Enrico Testa <enrico@mailtesta.com>
2019-10-17 06:31:50 +0200

    add preprocess atoms string2int + onehot

--- create_atoms.py
+++ create_atoms.py
@@ -53,6 +53,28 @@ for scorer in data['SCORING'].keys():
     # TODO: Test atom locally on sample data
     pass
 
+for encoder in data["PREPROCESSING"].keys():
+
+    # Notification
+    print("Building docker container for: %s" % encoder)
+
+    # Build shell command
+    cmd = ["cd preprocess",
+           "sudo docker build -f " + data['PREPROCESSING'][encoder]['docker_file'] + " -t " +
+           get_image_uri('PREPROCESSING', encoder) + " ./"]
+
+    # Execute command
+    try:
+        check_call(' && '.join(cmd), shell=True)
+        successful_builds.append(encoder)
+        successful_image_uris.append(get_image_uri('SCORING', encoder))
+    except CalledProcessError as e:
+        print("Failed to execute subprocess for container %s. Return code is %s." % (encoder, e.returncode))
+        failed_builds.append(encoder)
+
+    # TODO: Test atom locally on sample data
+    pass
+
 # Summary
 print("")
 print("")
