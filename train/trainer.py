c7a0043dce7580a582a8bb7c9968b1fc8bac7eab
Enrico Testa <enrico@mailtesta.com>
2019-10-25 07:32:35 +0200

    add imbalance treatment

--- train/trainer.py
+++ train/trainer.py
@@ -4,6 +4,7 @@ from shutil import rmtree
 from pandas import DataFrame, concat
 from sklearn.model_selection import StratifiedKFold, KFold, PredefinedSplit, cross_validate
 from sklearn import metrics
+from imblearn.under_sampling import RandomUnderSampler
 from atoms import Atom
 
 
@@ -13,9 +14,25 @@ CLASSIFICATION_ESTIMATORS = ['LogisticRegression', 'LGBMClassifier',
 REGRESSION_ESTIMATORS = ['LinearRegression']
 BENCHMARK_ESTIMATORS = ['DummyClassifier']
 CV_FOLDS = 3
+IMBALANCE_TOLERANCE = 0.01
 HYPERTUNE_LOSSES = {'binary_crossentropy': 'log_loss',
                     'categorical_crossentropy': 'log_loss',
                     'accuracy': 'accuracy'}
+
+
+class ImbalancedPredefinedSplit(PredefinedSplit):
+
+    def __init__(self, test_fold=None):
+        super().__init__(test_fold)
+
+    def split(self, X=None, y=None, groups=None):
+        rus = RandomUnderSampler(sampling_strategy='not minority', replacement=False)
+        for original_train_index, original_test_index in super(ImbalancedPredefinedSplit, self).split(X, y, groups):
+            resampled_train_index, _ = rus.fit_resample(X.index[original_train_index].values.reshape(-1, 1),
+                                                        y.iloc[original_train_index])
+            resampled_mask = [item in resampled_train_index for item in X.index]
+            resampled_train_rows = [i for i, item in enumerate(resampled_mask) if item is True]
+            yield np.asarray(resampled_train_rows), original_test_index
 
 
 class Trainer(Atom):
@@ -83,13 +100,16 @@ class Trainer(Atom):
             d['type'] = 'classification'
         if d['type'] == 'classification':
             d['binary'] = len(yu) == 2
-            d['balanced'] = bool(yu_counts.max() == yu_counts.min())
+            response_values = y.value_counts(normalize=True)
+            d['balanced'] = bool((response_values.max() - response_values.min())/2 <= IMBALANCE_TOLERANCE)
         return d
 
     def generate_folds(self, x, y, stratification=None):
         # TODO: manage shuffle without messing up with original DataFrame indexes
         test_folds = -np.ones(y.shape)
         counter = 0
+
+        # Stratification
         if stratification is None:
             for _, test_index in KFold(n_splits=CV_FOLDS, shuffle=False).split(x):
                 test_folds[test_index] = counter
@@ -97,7 +117,6 @@ class Trainer(Atom):
         else:
             # Build stratification variable
             tmp_var = stratification.astype(str)
-            tmp_var['target'] = y.astype(str)
             for column in tmp_var.columns:
                 try:
                     strat_var += tmp_var[column]
@@ -107,7 +126,15 @@ class Trainer(Atom):
             for _, test_index in StratifiedKFold(n_splits=CV_FOLDS, shuffle=False).split(x, strat_var):
                 test_folds[test_index] = counter
                 counter += 1
-        return PredefinedSplit(test_folds)
+
+        # Imbalance
+        if self.problem_specs["type"] == "classification":
+            if self.problem_specs["balanced"]:
+                return PredefinedSplit(test_folds)
+            else:
+                return ImbalancedPredefinedSplit(test_folds)
+        else:
+            return PredefinedSplit(test_folds)
 
     def generalization_assessment(self, x, y, cv):
         validation = cross_validate(self.algo(**self.params['algo']), x, y=y, scoring=self._get_scoring_list(y),
@@ -159,7 +186,13 @@ class Trainer(Atom):
         self.validation, self.predictions = self.generalization_assessment(x, y, cv=cv)
 
         # Fit model on entire train data
-        self.trained_model = self.fit(x, y)
+        if self.problem_specs["type"] == "classification" and not self.problem_specs["balanced"]:
+            rus = RandomUnderSampler(sampling_strategy='not minority', replacement=False)
+            resampled_index, _ = rus.fit_resample(x.index.values.reshape(-1, 1), y)
+            resampled_index = resampled_index.flatten()
+            self.trained_model = self.fit(x.loc[resampled_index, :], y.loc[resampled_index])
+        else:
+            self.trained_model = self.fit(x, y)
 
         # Export
         unique_id = self.generate_unique_id()
