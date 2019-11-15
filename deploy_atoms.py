20e0a099f26dd3aaf6db2a3ca3835fba436bf1df
Enrico Testa <enrico@mailtesta.com>
2019-11-15 19:34:10 +0100

    fix

--- deploy_atoms.py
+++ deploy_atoms.py
@@ -10,27 +10,27 @@ with open(os.getcwd() + "/config/atoms.yml", 'r') as stream:
 
 # TODO: fix permission command to push containers automatically to container registry. Currently must push manually.
 # Get permission to push to registry
-try:
-    # This was successful when sent manually
-    "cat ~/mlatoms/deploy/container_registry_ref_file.json | docker login -u _json_key --password-stdin https://gcr.io"
-
-    # If you try to do it again it says
-    "docker login requires at most 1 argument"
-
-    cmd = "cat " + GLOBALS["STORAGE_SA"] + \
-          " | docker login -u _json_key --password-stdin https://" + GLOBALS['CONTAINERS_ROOT_URL']
-
-    cmd_2 = 'docker login -u _json_key -p \"$(cat ' + GLOBALS["STORAGE_SA"] + \
-            ')\" https://' + GLOBALS['CONTAINERS_ROOT_URL']
-
-    print("")
-    print(cmd_2)
-    print("")
-    check_call(cmd)
-except CalledProcessError as e:
-    print("Failed to obtain access rights to repository. Subprocess return code is %s" % e.returncode)
-    print("Aborting...")
-    exit()
+# try:
+#     # This was successful when sent manually
+#     "cat ~/mlatoms/deploy/container_registry_ref_file.json | docker login -u _json_key --password-stdin https://gcr.io"
+#
+#     # If you try to do it again it says
+#     "docker login requires at most 1 argument"
+#
+#     cmd = "cat " + GLOBALS["STORAGE_SA"] + \
+#           " | docker login -u _json_key --password-stdin https://" + GLOBALS['CONTAINERS_ROOT_URL']
+#
+#     cmd_2 = 'docker login -u _json_key -p \"$(cat ' + GLOBALS["STORAGE_SA"] + \
+#             ')\" https://' + GLOBALS['CONTAINERS_ROOT_URL']
+#
+#     print("")
+#     print(cmd_2)
+#     print("")
+#     check_call(cmd)
+# except CalledProcessError as e:
+#     print("Failed to obtain access rights to repository. Subprocess return code is %s" % e.returncode)
+#     print("Aborting...")
+#     exit()
 
 successful_pushes = []
 failed_pushes = []
