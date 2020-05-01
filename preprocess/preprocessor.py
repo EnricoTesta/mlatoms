adb3da72ad9ab284f8ef08c6b9617aed3ae53ab0
Enrico Testa <enrico@mailtesta.com>
2020-05-01 07:30:32 +0200

    remove Encoder class

--- preprocess/preprocessor.py
+++ preprocess/preprocessor.py
@@ -4,31 +4,6 @@ from atoms import Atom
 import os
 
 
-class Encoder(Atom):
-
-    def __init__(self, data_path=None, model_path=None, algo=None, params=None):
-        super().__init__(data_path, model_path, algo, params)
-
-    def run(self):
-
-        # Fetch data
-        self.retrieve_data()
-        self.read_info()
-        self.read_data()
-
-        # Fit model
-        self.trained_model = self.fit(self.data, None)
-        self.predictions = self.transform(self.trained_model, self.data).reset_index()  # TODO: verify index
-
-        # Export
-        unique_id = self.generate_unique_id()
-        self.export_file(self.trained_model, 'model_' + unique_id + '.pkl')
-        self.export_file(self.predictions, 'predictions_' + unique_id + '.csv')
-
-        # Clean-up
-        rmtree(self.local_path)
-
-
 class DataEvaluator(Atom):
 
     def __init__(self, data_path=None, model_path=None, algo=None, params=None):
