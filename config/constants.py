db0050d2f47db80502ce8ca4baec62829480ce2c
Enrico Testa <enrico@mailtesta.com>
2020-05-01 08:50:43 +0200

    remove

--- config/constants.py
+++
@@ -1,8 +0,0 @@
-from yaml import safe_load
-import os
-
-PATH = os.path.abspath(os.path.dirname(__file__))
-
-# Import global GCP deployment settings from main configuration file
-with open("/gauth/ap_account_deployment.yml", 'r') as f:
-    GLOBALS = safe_load(f)
