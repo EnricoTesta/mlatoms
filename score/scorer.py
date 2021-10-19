2247f9c4d823c37140c2466af9b816a61366fe77
Enrico Testa <enrico@mailtesta.com>
2021-10-19 07:08:33 +0200

    read only columns needed from model_metadata file + make neutralize_scores() more efficient (acts on a subset of features)

--- score/scorer.py
+++ score/scorer.py
@@ -2,9 +2,10 @@ import os
 import pickle
 import subprocess
 import numpy as np
+from yaml import safe_load
 from scipy import stats
 from shutil import rmtree
-from pandas import DataFrame
+from pandas import DataFrame, read_csv
 from logging import getLogger
 from sklearn.preprocessing import MinMaxScaler
 from atoms import Atom, squeeze_proba
@@ -19,6 +20,8 @@ class BatchPredictor(Atom):
         self.preprocess_path = preprocess_path  # this is either a string or a list of strings
         self.stratification_df = None
         self._model = None
+        self.model_metadata = None
+        self.model_feature_importance = None
         self._preprocessor = None
         self._output_dir = output_dir
         self._use_probabilities = use_probabilities
@@ -69,7 +72,8 @@ class BatchPredictor(Atom):
     def retrieve_model(self):
         # Fetch model & preprocess from GCS
         try:
-            os.system(' '.join(['gsutil -m', 'cp', self.model_path, self.local_path]))
+            remote_model_path = "/".join(self.model_path.split("/")[0:-1])
+            os.system(' '.join(['gsutil -m', 'cp -r', remote_model_path, self.local_path]))
         except:
             raise ValueError("Model file not found")
 
@@ -90,10 +94,39 @@ class BatchPredictor(Atom):
     def restore_model(self):
         # Restore model
         try:
-            with open(os.path.join(self.local_path, self.model_path.split("/")[-1]), 'rb') as f:
+            with open(os.path.join(self.local_path, self.model_path.split("/")[-2], self.model_path.split("/")[-1]), 'rb') as f:
                 self._model = pickle.load(f)
         except:
             raise ValueError("Unable to unpickle model file")
+
+    def read_feature_importances(self):
+
+        model_featimp_path = "".join([self.local_path, self.model_path.split("/")[-2], "/"])
+        file_list = [item for item in os.listdir(model_featimp_path)
+                     if os.path.isfile(os.path.join(model_featimp_path, item)) and
+                     item.endswith('csv') and item.startswith('featimp')]
+        if len(file_list) > 1:
+            raise ValueError("Found multiple feature importance files. You must provide a single model feature importance file.")
+        local_featimp_path = os.path.join(model_featimp_path, file_list[0])
+        try:
+            self.model_feature_importance = read_csv(local_featimp_path)
+        except FileNotFoundError:
+            logger.warning("Feature importance not available.")
+
+
+    def read_model_metadata(self):
+        """
+        Load model metadata in memory. Raise exception if operation fails.
+        """
+        model_metadata_path = "".join([self.local_path, self.model_path.split("/")[-2], "/"])
+        file_list = [item for item in os.listdir(model_metadata_path)
+                     if os.path.isfile(os.path.join(model_metadata_path, item)) and
+                     item.endswith('json') and item.startswith('params')]
+        if len(file_list) > 1:
+            raise ValueError("Found multiple model parameter files. You must provide a single model parameter file.")
+        local_params_path = os.path.join(model_metadata_path, file_list[0])
+        with open(local_params_path, 'r') as stream:
+            self.model_metadata = safe_load(stream)
 
     def model_predict_proba(self, inputs):
         return inputs
@@ -103,8 +136,11 @@ class BatchPredictor(Atom):
 
     @staticmethod
     def _neutralize(df, columns, by, proportion=1.0):
-        scores = df[columns]
-        exposures = df[by].values
+        df_32 = df.astype('float32', copy=True) # ensure data type compatibility with np.linalg
+        scores = df_32[columns]
+        exposures = df_32[by].values
+
+        # Very expensive as exposures increase in number
         scores = scores - proportion * exposures.dot(np.linalg.pinv(exposures).dot(scores))
         return scores / scores.std()
 
@@ -127,7 +163,18 @@ class BatchPredictor(Atom):
         squeezed_scores = squeeze_proba(scores.set_index('id'), index=True)
 
         # Merge in a single dataframe
-        features = self.data.columns
+        self.read_feature_importances()
+        # Select features to neutralize on. At most 300 features to neutralize on.
+        if self.model_feature_importance is not None:
+            relevant_features_df = self.model_feature_importance.loc[self.model_feature_importance['feature_importance'] > 0].sort_values('feature_importance', ascending=False)
+            if relevant_features_df.shape[1] <= 300:
+                features = list(relevant_features_df['feature_name'])
+            else:
+                features = list(relevant_features_df['feature_name'].iloc[0:300])
+        elif self.data.columns <= 300:
+            features = self.data.columns
+        else:
+            features = self.data.columns[0:300]
         df = self.data.merge(squeezed_scores, left_index=True, right_index=True)\
             .merge(self.stratification_df, left_index=True, right_index=True)
 
@@ -152,11 +199,15 @@ class BatchPredictor(Atom):
         self.retrieve_metadata()
         self.read_info()
         self.read_metadata()
+        self.retrieve_preprocess()
+        self.retrieve_model()
+        self.read_model_metadata()
+        strat_column = self.info["STRATIFICATION_COLUMN"] if "STRATIFICATION_COLUMN" in self.info.keys() else []
+        cols_to_read = [self.info["ID_COLUMN"]] + strat_column + self.model_metadata['features']
+        self.params['read']['columns'] = cols_to_read # read only feature on which the model was trained on
         self.read_data(**self.params['read'])
 
         # Load model
-        self.retrieve_preprocess()
-        self.retrieve_model()
         self.restore_preprocess()
         self.restore_model()
 
@@ -192,9 +243,11 @@ class BatchPredictor(Atom):
         del ids
 
         # Neutralize scores (a.k.a. remove linear exposures with features)
+        logger.info("Neutralizing scores...")
         neutralized_scores = self.neutralize_scores(scores)
 
         # Write scores
+        logger.info("Writing scores...")
         scores.to_csv(path_or_buf=tmp_file_path, index=False)
         neutralized_scores.to_csv(path_or_buf=neutral_tmp_file_path, index=False)
 
@@ -203,9 +256,9 @@ class BatchPredictor(Atom):
                                os.path.join(self._output_dir, 'results.csv')])
         if self._output_dir.startswith("gs://"):
             shards = self._output_dir.split("/")
-            prefix = shards[0:-4]
-            neutral_folder = ['NEUTRALIZED_' + shards[-4]]
-            suffix = shards[-3:]
+            prefix = shards[0:-2]
+            neutral_folder = ['NEUTRALIZED_' + shards[-2]] #TODO: fix this path
+            suffix = [''] # shards[-3:]
             full_neutralized_path = os.path.join("/".join(prefix + neutral_folder + suffix), 'neutralized_results.csv')
         else:
             full_neutralized_path = os.path.join("/".join(["/".join(self._output_dir.split("/")[0:-2]),
